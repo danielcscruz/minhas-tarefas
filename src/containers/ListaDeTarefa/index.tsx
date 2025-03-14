736c452373dc8f2a0d08f3213f03278bdc39a3e9
danielcruz <danielcscruz@gmail.com>
2025-03-14 01:25:58 -0300

    added redux

--- src/containers/ListaDeTarefa/index.tsx
+++ src/containers/ListaDeTarefa/index.tsx
@@ -1,7 +1,30 @@
-const ListaDeTarefas = () => (
-  <main>
-    <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
-  </main>
-)
+import { useSelector } from 'react-redux'
+
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+import * as enums from '../../utils/enums/tarefa'
+import { RootReducer } from '../../store'
+
+const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
+
+  return (
+    <Container>
+      <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              descricao={t.descricao}
+              titulo={t.titulo}
+              status={t.status}
+              prioridade={t.prioridade}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
 
 export default ListaDeTarefas
---
+++ src/store/reducers/tarefas.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import Tarefa from '../../models/Tarefa'
+import * as enums from '../../utils/enums/tarefa'
+
+const tarefasSlice = createSlice({
+  name: 'tarefas',
+  initialState: [
+    new Tarefa(
+      'Estudar JS',
+      enums.Prioridade.IMPORTANTE,
+      enums.Status.PENDENTE,
+      '',
+      1
+    ),
+    new Tarefa(
+      'Estudar typescript',
+      enums.Prioridade.URGENTE,
+      enums.Status.CONCLUIDA,
+      'rever aula',
+      2
+    ),
+    new Tarefa(
+      'Estudar react',
+      enums.Prioridade.URGENTE,
+      enums.Status.PENDENTE,
+      'pratica useEffect',
+      3
+    )
+  ],
+  reducers: {
+    remover: (state, action: PayloadAction<number>) => {
+      state = state.filter((tarefa) => tarefa.id !== action.payload)
+    }
+  }
+})
+
+export const { remover } = tarefasSlice.actions
+
+export default tarefasSlice.reducer
